b962a2c3d9cd6d3bb690801a380d65cd506afb93
Artyom Doronin <UTF-16@yandex.ru>
2026-06-19 12:28:29 +0300

    ап

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/task-manager/',
+  base: '/registry/',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
