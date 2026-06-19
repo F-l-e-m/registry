a53273eefeb37383a527cb4dc60841e677f33ed0
Artyom Doronin <UTF-16@yandex.ru>
2026-06-19 12:30:43 +0300

    ап

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/registry/',
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-  },
   plugins: [react()],
 })
